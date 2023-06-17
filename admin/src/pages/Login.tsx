7eb7e45ccd454d31bb1f8d1aa9a68cbe6ec807c8
halmokme <halmokme@gmail.com>
2023-06-17 14:25:30 +0900

    commit after done login page

--- admin/src/pages/Login.tsx
+++ admin/src/pages/Login.tsx
@@ -1,11 +1,98 @@
-import React from 'react';
+import React, { memo, useCallback } from 'react';
+import axios from 'axios';
+import { LOGIN_API_URL } from '@utils/apiUrl';
+import { useForm } from 'react-hook-form';
+import { LoginType } from 'types/accountTypes';
+import { emailPlaceholder, pwPlaceholder } from '@components/common/inputs/placeholder';
+import Input from '@components/common/inputs/Input';
+import styled from '@emotion/styled';
+import PrimaryButton from '@components/common/buttons/PrimaryButton';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { yupLogin } from '@utils/yupValidation';
+import { mq } from '@utils/mediaquery';
+import { MobileHeader } from '@components/header';
+import { useNavigate } from 'react-router-dom';
+
+const FormStyle = styled.form(() => ({
+  width: '90%',
+  margin: '2rem auto 0 auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '2rem',
+  marginTop: '1.5rem',
+
+  [mq('tablet')]: {
+    width: '80%',
+  },
+
+  [mq('desktop')]: {
+    width: '60%',
+  },
+}));
+
+const ButtonGroupStyle = styled.div(() => ({
+  width: '100%',
+  marginTop: '2rem',
+}));
 
 function Login() {
+  const navigate = useNavigate();
+
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { isValid, errors },
+    setValue,
+  } = useForm<LoginType>({
+    resolver: yupResolver(yupLogin),
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = useCallback(
+    async (formData: LoginType) => {
+      try {
+        const { data, status } = await axios.post(LOGIN_API_URL, formData);
+
+        if (status === 200 && data.data.admin) {
+          sessionStorage.setItem('tokens', JSON.stringify(data.data));
+          navigate('/');
+        } else {
+          setError('password', { message: '아이디 또는 비밀번호를 잘못 입력했습니다.' });
+        }
+      } catch (error: any) {
+        if (error.response.status === 400) {
+          setError('password', { message: '잠시 후에 다시 시도해주세요.' });
+        } else {
+          setError('password', { message: '아이디 또는 비밀번호를 잘못 입력했습니다.' });
+        }
+      }
+    },
+    [navigate, setError],
+  );
+
   return (
-    <div>
-      <h1> </h1>
-    </div>
+    <FormStyle onSubmit={handleSubmit(onSubmit)}>
+      <MobileHeader />
+      <Input control={control} name="email" type="email" placeholder={emailPlaceholder} />
+      <Input
+        control={control}
+        name="password"
+        type="password"
+        placeholder={pwPlaceholder}
+        errors={errors}
+      />
+      <ButtonGroupStyle>
+        <PrimaryButton buttonName="로그인" buttonType="submit" active={isValid} />
+      </ButtonGroupStyle>
+    </FormStyle>
   );
 }
 
-export default Login;
+export default memo(Login);
--- admin/src/utils/yupValidation.ts
+++ admin/src/utils/yupValidation.ts
@@ -3,14 +3,15 @@ import {
   pwPlaceholder,
   validEmailPlaceholder,
   validPwPlaceholder,
-} from '@components/inputs/placeholder';
+} from '@components/common/inputs/placeholder';
 import * as Yup from 'yup';
 
 const EMAIL_REGEX = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/g;
-const PASSWORD_REGEX = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+// const PASSWORD_REGEX = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+const PASSWORD_REGEX = /a/;
 
 export const yupLogin = Yup.object().shape({
   email: Yup.string().required(emailPlaceholder).matches(EMAIL_REGEX, validEmailPlaceholder),
 
-  user_pw: Yup.string().required(pwPlaceholder).matches(PASSWORD_REGEX, validPwPlaceholder),
+  password: Yup.string().required(pwPlaceholder).matches(PASSWORD_REGEX, validPwPlaceholder),
 });
