85370f71fa12d6a75571d48e20387fd21dbfd2df
halmokme <halmokme@gmail.com>
2023-06-17 13:04:28 +0900

    commit after modify webHeader

--- admin/src/components/header/WebHeader.tsx
+++ admin/src/components/header/WebHeader.tsx
@@ -1,12 +1,22 @@
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
+import { mq } from '@utils/mediaquery';
 import React from 'react';
 
-const Container = styled.div(() => ({
+const Container = styled.div(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   gap: '1.5rem',
   height: '6rem',
+  paddingLeft: theme.spacing.mobPadding,
+
+  [mq('tablet')]: {
+    paddingLeft: theme.spacing.tabPadding,
+  },
+
+  [mq('desktop')]: {
+    paddingLeft: theme.spacing.pcPadding,
+  },
 }));
 
 const LogoText = styled.div(({ theme }) => ({
