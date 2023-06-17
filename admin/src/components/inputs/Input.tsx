fc2d138c42847b7b18cd8f1332e042d4db4475e9
halmokme <halmokme@gmail.com>
2023-06-17 13:25:07 +0900

    commit after modify input comp

--- admin/src/components/inputs/Input.tsx
+++ admin/src/components/inputs/Input.tsx
@@ -1,16 +1,23 @@
-import { Control, Controller } from 'react-hook-form';
+import { Control, Controller, FieldErrors } from 'react-hook-form';
 import React from 'react';
-import { LoginType } from 'types/userTypes';
+import { LoginType } from 'types/accountTypes';
 import styled from '@emotion/styled';
 
 interface InputProps {
   control: Control<LoginType>;
-  name: 'email' | 'user_pw';
+  name: 'email' | 'password';
   type: string;
   placeholder: string;
+  errors?: FieldErrors<LoginType> | undefined;
 }
 
-const InputStyle = styled.div(({ theme }) => ({
+const InputWrapper = styled.div(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+}));
+
+const InputContainer = styled.div(({ theme }) => ({
   width: '100%',
   padding: '0.8rem',
   border: `1px solid ${theme.color.gray300}`,
@@ -20,15 +27,25 @@ const InputStyle = styled.div(({ theme }) => ({
   },
 }));
 
-function Input({ control, name, type, placeholder, ...rest }: InputProps) {
+const ErrorMessage = styled.div(({ theme }) => ({
+  color: theme.color.red500,
+  fontSize: '1.2rem',
+  textAlign: 'left',
+  paddingTop: '1rem',
+}));
+
+function Input({ control, name, type, placeholder, errors }: InputProps) {
   return (
-    <InputStyle>
-      <Controller
-        control={control}
-        name={name}
-        render={({ field }) => <input type={type} {...field} placeholder={placeholder} />}
-      />
-    </InputStyle>
+    <InputWrapper>
+      <InputContainer>
+        <Controller
+          control={control}
+          name={name}
+          render={({ field }) => <input type={type} {...field} placeholder={placeholder} />}
+        />
+      </InputContainer>
+      <ErrorMessage>{errors?.password && <p>{errors.password.message}</p>}</ErrorMessage>
+    </InputWrapper>
   );
 }
 
