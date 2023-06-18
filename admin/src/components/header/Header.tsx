87194b50db8bbf0d719b9a34fa02867349aeb61d
halmokme <halmokme@gmail.com>
2023-06-18 18:12:12 +0900

    commit after remove unnecessary code

--- admin/src/components/header/Header.tsx
+++ admin/src/components/header/Header.tsx
@@ -1,16 +1,10 @@
-import { IsDefault } from '@utils/mediaquery';
 import React from 'react';
 import WebHeader from './WebHeader';
-import MobileHeader from './MobileHeader';
 
 function Header() {
   return (
     <>
       <WebHeader />
-
-      {/* <IsDefault>
-      <MobileHeader />
-      </IsDefault> */}
     </>
   );
 }
