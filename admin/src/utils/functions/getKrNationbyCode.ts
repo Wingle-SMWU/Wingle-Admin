b771f6d20a051e821943d5102a590de5d8d654d3
halmokme <halmokme@gmail.com>
2023-07-24 02:52:09 +0900

    during qa

--- admin/src/utils/functions/getKrNationbyCode.ts
+++ admin/src/utils/functions/getKrNationbyCode.ts
@@ -4,7 +4,9 @@ const getKrNationByCode = (code: string): string | undefined => {
   if (!code) return undefined;
 
   const country = countryList.find((country) => country.code === code);
-  return country?.krNation;
+  const countryTwo = countryList.find((country) => country.enNation === code);
+
+  return country?.krNation || countryTwo?.krNation;
 };
 
 export default getKrNationByCode;
